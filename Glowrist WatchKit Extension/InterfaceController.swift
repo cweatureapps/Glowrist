7eaadcc9f0574dbcb9207fde50cbde0e8d26ef5e
Ken Ko <ken.ko.au@gmail.com>
2016-05-15 19:29:11 +1000

    Initial commit for new project

---
+++ Glowrist WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Glowrist WatchKit Extension
+//
+//  Created by Ken Ko on 15/05/2016.
+//  Copyright © 2016 Ken Ko. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
